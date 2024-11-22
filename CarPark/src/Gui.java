3fb6572fd7f0f5d54cac2cc19b73f82e95103cb4
Jake Orton <116884079+Jxkeorton@users.noreply.github.com>
2024-11-22 14:16:48 +0000

    GUI Setup 2

--- CarPark/src/Gui.java
+++ CarPark/src/Gui.java
@@ -2,6 +2,14 @@ import javax.swing.*;
 
 public class GUI extends App {
     public MainScreen(){
-        
+        //Liz
+    };
+
+    public LogInScreen(){
+        //Lee
+    };
+
+    public AdminScreen(){
+        // Jake
     };
 };
